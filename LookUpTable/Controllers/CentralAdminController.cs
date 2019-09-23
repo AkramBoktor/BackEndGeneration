2d1b9a1960c78ffc9406edf662a57d208eaf5617
Abdelrahman Hady <abdelrahmanhadii@hotmail.com>
2019-09-23 16:09:21 +0200

    lookUpTableCreation

---
+++ LookUpTable/Controllers/CentralAdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LookUpTable.DAL;
+using LookUpTable.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LookUpTable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CentralAdminController : ControllerBase
+    {
+        private readonly CentralAdminDbContext _context;
+        public CentralAdminController(CentralAdminDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<CentralAdministration>> GetAll()
+        {
+            return await _context.CentralAdministrations.ToListAsync();
+        }
+        [HttpGet("{Id}")]
+        public async Task<CentralAdministration> GetById([FromRoute] int Id)
+        {
+            return await _context.CentralAdministrations.FirstOrDefaultAsync(a => a.Id == Id);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CentralAdministration centralAdministration)
+        {
+            await _context.AddAsync(centralAdministration);
+            var res = await _context.SaveChangesAsync();
+            if (res > 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] CentralAdministration centralAdministration)
+        {
+            _context.Entry(centralAdministration).State = EntityState.Modified;
+            var res = await _context.SaveChangesAsync();
+            if (res > 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ LookUpTable/DAL/CentralAdminDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LookUpTable.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookUpTable.DAL
+{
+    public class CentralAdminDbContext:DbContext
+    {
+        public CentralAdminDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<CentralAdministration> CentralAdministrations { get; set; }
+    }
+}
